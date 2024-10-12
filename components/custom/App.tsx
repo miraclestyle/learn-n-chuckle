d0700a3e03cda96d127643df865db36e6544f97d
elvinkosova <elvinkosova@gmail.com>
2024-10-12 23:11:21 +0000

    Fix bug

--- components/custom/App.tsx
+++ components/custom/App.tsx
@@ -20,8 +20,7 @@ import { useTextGenerator, useSpeechGenerator } from '@/hooks'
 const App = () => {
   const [file, setFile] = useState<string | null>(null)
   const { text, generateText, isGeneratingText } = useTextGenerator()
-  const { audioUrl, generateSpeech, isGeneratingSpeech, resetUrl } =
-    useSpeechGenerator()
+  const { audioUrl, generateSpeech, isGeneratingSpeech } = useSpeechGenerator()
 
   const generate = async (data: IPrompt) => {
     const content = await generateText(data)
@@ -44,15 +43,17 @@ const App = () => {
           {isGeneratingText && <Text>{generatingMessage}</Text>}
           {!isGeneratingText && !text && <Text>{defaultMessage}</Text>}
           {!isGeneratingText && text && <Text>{text}</Text>}
-          {!isGeneratingText && !isGeneratingSpeech && text && audioUrl && !file && (
-            <Button onClick={() => setFile(audioUrl)}>Play</Button>
-          )}
-          {!isGeneratingText && !isGeneratingSpeech && text && audioUrl && file && (
-            <Button onClick={() => setFile(null)}>Stop</Button>
-          )}
-          {file && (
-            <Audio src={file} autoPlay onEnded={() => setFile(null)} />
-          )}
+          {!isGeneratingText &&
+            !isGeneratingSpeech &&
+            text &&
+            audioUrl &&
+            !file && <Button onClick={() => setFile(audioUrl)}>Play</Button>}
+          {!isGeneratingText &&
+            !isGeneratingSpeech &&
+            text &&
+            audioUrl &&
+            file && <Button onClick={() => setFile(null)}>Stop</Button>}
+          {file && <Audio src={file} autoPlay onEnded={() => setFile(null)} />}
         </Content>
         <Footer>Happy Prompting, Happy Roasting! from Promptlys !</Footer>
         <Image
