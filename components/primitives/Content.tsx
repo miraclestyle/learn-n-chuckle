b7cba1065b77e08d0b63655584d26c61bf459fdf
elvinkosova <elvinkosova@gmail.com>
2024-10-13 17:40:59 +0000

    Revert colors to previous

--- components/primitives/Content.tsx
+++ components/primitives/Content.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 
 const Content = ({ children }: Props) => (
-  <div className="flex flex-col gap-8 items-center justify-center bg-amber-400 rounded-2xl p-6">
+  <div className="flex flex-col gap-8 items-center justify-center bg-amber-300 rounded-2xl p-6">
     {children}
   </div>
 )
