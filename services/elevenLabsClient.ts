f55e26b0d552ed906842f719e5d8d698279683fe
elvinkosova <elvinkosova@gmail.com>
2024-10-13 16:26:17 +0000

    Improve services code organization

---
+++ services/elevenLabsClient.ts
@@ -0,0 +1,27 @@
+'use server'
+
+import { ElevenLabsClient } from 'elevenlabs'
+import { Readable } from 'stream'
+
+const client = new ElevenLabsClient({
+  apiKey: process.env.ELEVENLABS_API_KEY,
+})
+
+
+export const generateSpeach = async (text: string): Promise<Readable> => {
+  const audio = await client.generate({
+    text,
+    voice: 'George',
+    model_id: 'eleven_turbo_v2_5',
+    voice_settings: {
+      stability: 0.3,
+      similarity_boost: 0.7,
+      style: 1.0,
+      use_speaker_boost: true,
+    },
+  })
+
+  return audio
+}
+
+
--- services/speechGenerator.ts
+++ services/speechGenerator.ts
@@ -1,36 +1,14 @@
 'use server'
-import { ElevenLabsClient } from 'elevenlabs'
-import { Readable } from 'stream'
 
+import { generateSpeach } from './elevenLabsClient'
 import { saveFile } from '@/services/supabaseClient'
-
-const client = new ElevenLabsClient({
-  apiKey: process.env.ELEVENLABS_API_KEY,
-})
 
 const removeBrackets = (text: string): string =>
   text.replace(/[\[\(].*?[\]\)]|\*\*.*?\*\*/g, '')
 
-
-const sendRequest = async (text: string): Promise<Readable> => {
-  const audio = await client.generate({
-    text,
-    voice: 'George',
-    model_id: 'eleven_turbo_v2_5',
-    voice_settings: {
-      stability: 0.3,
-      similarity_boost: 0.7,
-      style: 1.0,
-      use_speaker_boost: true,
-    },
-  })
-
-  return audio
-}
-
 const speechGenerator = async (text: string): Promise<string> => {
   const formattedText = removeBrackets(text)
-  const file = await sendRequest(formattedText)
+  const file = await generateSpeach(formattedText)
   const filePath = await saveFile(file)
   return filePath
 }
